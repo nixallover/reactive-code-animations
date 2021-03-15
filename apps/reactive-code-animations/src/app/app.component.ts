e21c4cfff6885b163ab2c80614865cabb809a168
Nicole Oliver <nicole@nrwl.io>
2021-03-15 14:56:23 -0700

    added simple menu

--- apps/reactive-code-animations/src/app/app.component.ts
+++ apps/reactive-code-animations/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  path: string;
+}
 @Component({
   selector: 'rca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  menuItems: MenuItem[] = [
+    { name: 'Nx Cloud Illustration', path: '/examples/nx-cloud-illustration' },
+    // more examples to come :)
+  ];
+}
